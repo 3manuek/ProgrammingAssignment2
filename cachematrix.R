ce82308088ee3f1ead56d4de76c44531c690c65d
Emanuel Calvo <3manuek@esdebian.org>
2015-06-20 22:54:29 -0300

    First commit, code works.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# source("cachematrix.R")
+# x <- matrix(rnorm(4), ncol=2)
+# z <- makeCacheMatrix(x)
+# cacheSolve(z)
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInvMatrix <- function(mean) m <<- mean
+        getInvMatrix <- function() m
+        list(set = set, get = get,
+             setInvMatrix = setInvMatrix,
+             getInvMatrix = getInvMatrix)
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInvMatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        #data <- x$get()
+        m <- solve(x$get())
+        x$setInvMatrix(m)
+        m
 }
